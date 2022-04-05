a2b662df1d2fb7a0f8340a04917518ffe0ab40c9
wsb05 <leo429@naver.com>
2022-04-05 10:45:50 +0900

    ex01.c

---
+++ ex01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define MAXLINE 100
+
+
+void copy(char from[],char to[]);
+char line[MAXLINE];
+char instr[MAXLINE];
+
+int main() {
+
+
+	return 0;
+}
+void copy(char from[],char to[]);
+
